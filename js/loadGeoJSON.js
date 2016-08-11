4eb6e0ce5ec6fce1672822d94de686605a70fcb2
sonalranjit <sonal.ranjit3@gmail.com>
2016-08-11 10:12:51 -0400

    michelle's work is validated again

--- js/loadGeoJSON.js
+++ js/loadGeoJSON.js
@@ -113,7 +113,7 @@ function addPointTypeLayer( data_url, id ) {
       }
     } );
   } );
-  /*
+  
   // When a click event occurs near a polygon, open a popup at the location of
   // the feature, with description HTML from its properties.
   map.on( 'click', function( e ) {
@@ -130,7 +130,7 @@ function addPointTypeLayer( data_url, id ) {
       .setLngLat( map.unproject( e.point ) )
       .setHTML( '<b>Name: </b>' + censusTract.properties.name )
       .addTo( map );
-  } );*/
+  } );
 
   // Use the same approach as above to indicate that the symbols are clickable
   // by changing the cursor style to 'pointer'.
